e68bc5fc71f49d095f171e1bde929c3e5e956ff2
jbatt <114751330+jbatt@users.noreply.github.com>
2024-09-07 13:30:57 +0100

    Added early stopper

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -65,3 +65,21 @@ def pad_to_square(x, size):
         padw = size - w
         x = F.pad(x, (0, padw, 0, padh))
         return x
+
+
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
