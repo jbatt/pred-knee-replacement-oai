553a20b46a71f40041093b1bbcf14dd9d8f80f40
jbatt <114751330+jbatt@users.noreply.github.com>
2025-02-11 12:00:50 +0000

    Increase memory allocation per CPU core in Slurm job configuration

--- src/scripts/train_model_slurm.sh
+++ src/scripts/train_model_slurm.sh
@@ -5,7 +5,7 @@
 
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=2G   # memory per cpu-core
+#SBATCH --mem-per-cpu=16G   # memory per cpu-core
 
 #SBATCH --time=48:00:00
 
