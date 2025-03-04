cd6ce3718dc30facf40e771936a1875e36b57bca
jbatt <114751330+jbatt@users.noreply.github.com>
2025-03-04 15:52:57 +0000

    Update training script to request 1 GPU and train 3d full res nnunet model using 5-fold cross-validation

--- src/scripts/train_nnunet_slurm.sh
+++ src/scripts/train_nnunet_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_nnunet
 #SBATCH --partition=gpu     # Request the GPU partition
-#SBATCH --gres=gpu:3        # Request 3 GPUs as 3 GPUs per node
+#SBATCH --gres=gpu:1        # Request 3 GPUs as 3 GPUs per node
 
 #SBATCH --ntasks-per-node=1 # Number of tasks per node - advice is to experiment with this value
 #SBATCH --cpus-per-task=8
@@ -17,5 +17,16 @@ module load cuda
 
 
 # Run the training script with the selected input file
-nnUNetv2_plan_and_preprocess -d 014 --verify_dataset_integrity
 
+# Initial planning and preprocessing
+# nnUNetv2_plan_and_preprocess -d 014 --verify_dataset_integrity
+
+# nnUnet model training
+
+# Set number of GPUs to 1
+export CUDA_VISIBLE_DEVICES=0
+
+# Train the 3d full res model for each fold
+for FOLD in 0 1 2 3 4; do
+    nnUNetv2_train 014 3d_fullres $FOLD --npz
+done
