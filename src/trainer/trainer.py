07ef40b6a253a02df8df5e11847b6b3e24012fc2
James Battye <scjb@leeds.ac.uk>
2025-04-16 11:20:11 +0100

    Added training loop dedicated to patch based training

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -69,7 +69,7 @@ def train_loop(
         # TODO: rmeove harcoded nmber of channels as 1
         if patch_size is not None:
             X = X.reshape(-1, 1, patch_size[0], patch_size[1], patch_size[2])
-        
+
         print(f"Image shape after combining patches into batch dimension: {X.size()}")
         
         # Compute prediction and loss
@@ -139,6 +139,163 @@ def train_loop(
         epoch_haus.append(hausdorff_distance_all.mean(dim=0))
 
         torch.cuda.empty_cache()
+
+    # Calculate the average loss and accuracy for the epoch
+    avg_epoch_loss = sum(epoch_loss) / len(epoch_loss)
+    avg_epoch_dice =  sum(epoch_dice) / len(epoch_loss) 
+    avg_epoch_haus = sum(epoch_haus) / len(epoch_loss)
+    
+    avg_epoch_dice_all = epoch_dice_all.mean(axis=0)
+    avg_epoch_haus_loss_all = epoch_haus_loss_all.mean(axis=0)
+    
+
+    return (avg_epoch_loss, avg_epoch_dice, avg_epoch_haus, 
+            avg_epoch_dice_all, avg_epoch_haus_loss_all)
+
+
+# Define a training loop function for training using patches for reuse later 
+def train_patch_loop(
+        dataloader, 
+        device, 
+        model, 
+        loss_fn, 
+        optimizer,
+        scaler, 
+        num_classes,
+        patch_size=None,
+    ):
+
+    print("Running training loop...")
+
+    # Release all unoccupied cached memory
+    gc.collect()
+    torch.cuda.empty_cache()
+    
+    # Load model to device
+    print(f"Loading model to device: {device}")
+    model.to(device)
+
+    # Initialise variables
+    epoch_loss = []
+    epoch_dice = []
+    epoch_haus = []
+
+    # Initialise separate dice/hausdorff array which will capture dice of all tissues indvidually
+    epoch_dice_all = np.empty(shape=(len(dataloader), num_classes))
+    epoch_haus_loss_all = np.empty(shape=(len(dataloader), num_classes))
+
+
+    size = len(dataloader.dataset)
+    print(f"Dataset size = {size}")
+
+
+    # Set the model to training mode
+    print(f"Setting model to train mode...")
+    model.train()
+
+    print("Starting training loop...")
+    
+    # For each batch from the data loader
+    for batch, (X_volume, y_volume) in enumerate(dataloader):
+
+        # Release all unoccupied cached memory
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        # If using patches combine patches into batch dimension (batch_size * num_patches, channels, patch_size[0], patch_size[1], patch_size[2])
+        # TODO: remove harcoded nmber of channels as 1
+        if patch_size is not None:
+            X_volume = X_volume.reshape(-1, 1, patch_size[0], patch_size[1], patch_size[2])
+            y_volume = y_volume.reshape(-1, num_classes, patch_size[0], patch_size[1], patch_size[2])
+            print(f"Image shape after combining patches into batch dimension: {X_volume.size()}")
+            print(f"Mask shape after combining patches into batch dimension: {y_volume.size()}")
+        
+        # TODO remove hardcoded patch batch size
+        patch_batch_size = 32
+
+        for i in range(0, X_volume.shape[0], patch_batch_size):
+
+            X = X_volume[i:i+patch_batch_size]
+            y = y_volume[i:i+patch_batch_size]
+            print(f"Image shape: {X.shape}")
+            print(f"Mask shape: {y.shape}")
+
+            # Load X and y to device
+            X = X.to(device)
+            y = y.to(device)
+
+            # Compute prediction and loss
+            with torch.autocast(device_type='cuda', dtype=torch.float16):
+                print("Computing model predictions...")
+                pred = model(X)
+                print(f"Prediction shape: {pred.shape}")
+                
+                # assert pred.dtype is torch.float16 - removed for now as amp may return float32 objects
+                print(f"Prediction type: {pred.dtype}")
+
+                print("Calculating loss...")
+                loss = loss_fn(pred, y)
+                assert loss.dtype is torch.float32
+            
+
+
+
+            # Reset gradients to zero
+            optimizer.zero_grad(set_to_none=True)
+
+            # Backpropagation
+            print("Computing backpropagation...")        
+            scaler.scale(loss).backward()
+
+            # Perform model step
+            scaler.step(optimizer)
+            scaler.update()
+
+
+            
+            # Store batch size
+            batch_size = len(y)
+
+            # # For every 5th batch, print the loss and current progress
+            if batch % 5 == 0:
+                loss, current = loss.item(), batch * batch_size + len(X)
+
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+            # Append the batch loss to enable calculation of the average epoch loss
+            epoch_loss.append(loss)
+            epoch_dice.append(dice_coefficient_multi_batch(pred, y).item())
+            epoch_dice_all[batch] = dice_coefficient_multi_batch_all(pred, y).detach().tolist()
+            
+
+            # Calculate Hausdorff distance:
+            # Turn model outputs from logits to onehot encoding required by hausdorff function
+            pred_softmax = torch.softmax(pred, dim=1)
+            print(f"Softmax shape: {pred_softmax.shape}")
+
+            pred_onehot = torch.zeros_like(pred_softmax)
+            print(f"Onehot shape: {pred_onehot.shape}")
+
+            # Scatter ones along the class dimension in to position of the max softmax value
+            pred_onehot.scatter_(1, pred_softmax.argmax(dim=1, keepdim=True), 1)
+            print(f"Onehot shape after scatter: {pred_onehot.shape}")
+
+
+            # Calculate Hausdorff distance for each class
+            # Remove dim of 1 ground truth
+            # Taking mean of Hausdorff distance of each class
+            # print(torch.squeeze(y, dim=1).shape))
+            
+            hausdorff_distance_all = compute_hausdorff_distance(pred_onehot, 
+                                                            torch.squeeze(y, dim=1),
+                                                            include_background=True).detach()
+
+            print(f"Hausdorff distance: {hausdorff_distance_all}")
+
+            epoch_haus_loss_all[batch] = hausdorff_distance_all.tolist()[0]
+            epoch_haus.append(hausdorff_distance_all.mean(dim=0))
+
+            torch.cuda.empty_cache()
 
     # Calculate the average loss and accuracy for the epoch
     avg_epoch_loss = sum(epoch_loss) / len(epoch_loss)
