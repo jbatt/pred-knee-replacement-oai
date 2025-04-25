ab24ef6fe79652fe682c62936f7df122025decd0
James Battye <scjb@leeds.ac.uk>
2025-04-25 10:39:44 +0100

    Added logic to handle different patching methods

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -175,7 +175,8 @@ def train_patch_loop(
         lr_scheduler,
         num_classes,
         patch_size=None,
-        patch_batch_size=32
+        patch_batch_size=32,
+        patch_method="grid_pytorch"
     ):
 
     print("Running training loop...")
@@ -216,22 +217,102 @@ def train_patch_loop(
         torch.cuda.empty_cache()
 
         # If using patches combine patches into batch dimension (batch_size * num_patches, channels, patch_size[0], patch_size[1], patch_size[2])
+        # Accumulate gradients over all patches in volume
+        
         # TODO: remove harcoded nmber of channels as 1
-        if patch_size is not None:
+        if patch_method == "grid_pytorch" and patch_size is not None:
+            
             X_volume = X_volume.reshape(-1, 1, patch_size[0], patch_size[1], patch_size[2])
             y_volume = y_volume.reshape(-1, num_classes, patch_size[0], patch_size[1], patch_size[2])
             print(f"Image shape after combining patches into batch dimension: {X_volume.size()}")
             print(f"Mask shape after combining patches into batch dimension: {y_volume.size()}")
         
+            print(f"\n\n Iterating over patches using a sub-batch size of {patch_batch_size}...\n\n")
 
-        print(f"\n\n Iterating over patches using a sub-batch size of {patch_batch_size}...\n\n")
+            # TODO: have this patch loop be for the  grid_pytorch method only 
+            for i in range(0, X_volume.shape[0], patch_batch_size):
+                
+                print(f"\n\nLoading patches {i}-{i+patch_batch_size}/{X_volume.shape[0]} to device...\n\n")
 
-        for i in range(0, X_volume.shape[0], patch_batch_size):
-            
-            print(f"\n\nLoading patches {i}-{i+patch_batch_size}/{X_volume.shape[0]} to device...\n\n")
+                X = X_volume[i:i+patch_batch_size]
+                y = y_volume[i:i+patch_batch_size]
+                print(f"Image shape: {X.shape}")
+                print(f"Mask shape: {y.shape}")
 
-            X = X_volume[i:i+patch_batch_size]
-            y = y_volume[i:i+patch_batch_size]
+                # Load X and y to device
+                X = X.to(device)
+                y = y.to(device)
+
+                # Compute prediction and loss
+                with torch.autocast(device_type='cuda', dtype=torch.float16):
+                    print("Computing model predictions...")
+                    pred = model(X)
+                    print(f"Prediction shape: {pred.shape}")
+                    
+                    # assert pred.dtype is torch.float16 - removed for now as amp may return float32 objects
+                    print(f"Prediction type: {pred.dtype}")
+
+                    # Calculate loss - normalised by number of patches in volume as gradients are accumulated
+                    print("Calculating loss...")
+                    loss = loss_fn(pred, y) / X_volume.shape[0]
+                    assert loss.dtype is torch.float32
+                
+
+                # Backpropagation - optimiser step is performed after all patches in volume have been processed (gradient accumulated)
+                print("Computing backpropagation...")        
+                scaler.scale(loss).backward()
+                
+                # Store batch size
+                batch_size = len(y)
+
+                # # For every 5th batch, print the loss and current progress
+                if batch % 5 == 0:
+                    loss, current = loss.item(), batch * batch_size + len(X)
+
+                    print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+                # Append the batch loss to enable calculation of the average epoch loss
+                epoch_loss.append(loss)
+                epoch_dice.append(dice_coefficient_multi_batch(pred, y).item())
+                epoch_dice_all[batch] = dice_coefficient_multi_batch_all(pred, y).detach().tolist()
+                
+
+                # Calculate Hausdorff distance:
+                # Turn model outputs from logits to onehot encoding required by hausdorff function
+                pred_softmax = torch.softmax(pred, dim=1)
+                print(f"Softmax shape: {pred_softmax.shape}")
+
+                pred_onehot = torch.zeros_like(pred_softmax)
+                print(f"Onehot shape: {pred_onehot.shape}")
+
+                # Scatter ones along the class dimension in to position of the max softmax value
+                pred_onehot.scatter_(1, pred_softmax.argmax(dim=1, keepdim=True), 1)
+                print(f"Onehot shape after scatter: {pred_onehot.shape}")
+
+
+                # Calculate Hausdorff distance for each class
+                # Remove dim of 1 ground truth
+                # Taking mean of Hausdorff distance of each class
+                # print(torch.squeeze(y, dim=1).shape))
+                
+                hausdorff_distance_all = compute_hausdorff_distance(pred_onehot, 
+                                                                torch.squeeze(y, dim=1),
+                                                                include_background=True).detach()
+
+                print(f"Hausdorff distance: {hausdorff_distance_all}")
+
+                epoch_haus_loss_all[batch] = hausdorff_distance_all.tolist()[0]
+                epoch_haus.append(hausdorff_distance_all.mean(dim=0))
+
+                torch.cuda.empty_cache()
+
+
+        # For random_monai approach just use normal dataloader / training loop
+        # Gradients are not accumulated
+        elif patch_method == "random_monai" and patch_size is not None:
+
+            X = X_volume
+            y = y_volume
             print(f"Image shape: {X.shape}")
             print(f"Mask shape: {y.shape}")
 
@@ -302,7 +383,8 @@ def train_patch_loop(
 
             torch.cuda.empty_cache()
 
-        # Perform model step - gradients have been accumulated over patch sub-batch
+
+        # Perform model step - gradients have been accumulated over patch sub-batch if using grid_pytorch patching method
         scaler.step(optimizer)
         scaler.update()            
         
