d81a97f9a64f4fd5e0e1bb3dbd4573fa8c3906c0
James Battye <scjb@leeds.ac.uk>
2025-08-28 14:29:47 +0100

    Cleaned up empty space

--- src/scripts/train_model_swinunetr_slurm.sh
+++ src/scripts/train_model_swinunetr_slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1 # Number of tasks per node - advice is to experiment with this value
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=16G   # memory per cpu-core
-
+``
 #SBATCH --time=48:00:00
 
 #SBATCH --mail-user=scjb@leeds.ac.uk # Email address for notifications
